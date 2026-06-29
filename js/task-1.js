30e11ddef8e6c2f4611a266ebe1d574e78d5a4b5
VL <victoria.lytvyn27@gmail.com>
2026-06-29 17:44:00 +0300

    fix task1

--- js/task-1.js
+++ js/task-1.js
@@ -1,23 +1,16 @@
-function isEnoughCapacity(products, containerSize){
+function isEnoughCapacity(products, containerSize) {
+  const productQuantities = Object.values(products);
   let total = 0;
-  for (let product in products) {
-    total += products[product];
+  for (const product of productQuantities) {
+    total += product;
   }
   return total <= containerSize;
 }
 
-console.log(
-  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-); // true
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
-console.log(
-  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-); // false
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
 
-console.log(
-  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-); // true
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
 
-console.log(
-  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-); // false
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
